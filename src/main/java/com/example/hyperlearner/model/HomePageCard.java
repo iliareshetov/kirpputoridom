c452113d5c9254ae2e049796947bf2adb5a64901
Saurabh <tawaresaurabh@gmail.com>
2019-06-07 19:21:45 +0300

    Added dynamic content for home page cards

--- src/main/java/com/example/hyperlearner/model/HomePageCard.java
+++ src/main/java/com/example/hyperlearner/model/HomePageCard.java
@@ -14,50 +14,17 @@ public class HomePageCard {
     @GeneratedValue
     @Column(name = "id")
     private Integer id;
-    @Column(name = "title")
-    private String title;
     @Column(name = "amount")
     private String amount;
-    @Column(name = "descriptionLine1")
-    private String descriptionLine1;
-    @Column(name = "descriptionLine2")
-    private String descriptionLine2;
-    @Column(name = "descriptionLine3")
-    private String descriptionLine3;
-    @Column(name = "descriptionLine4")
-    private String descriptionLine4;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "title")
+    private String title;
 
-    public String getDescriptionLine1() {
-        return descriptionLine1;
-    }
 
-    public void setDescriptionLine1(String descriptionLine1) {
-        this.descriptionLine1 = descriptionLine1;
-    }
 
-    public String getDescriptionLine2() {
-        return descriptionLine2;
-    }
 
-    public void setDescriptionLine2(String descriptionLine2) {
-        this.descriptionLine2 = descriptionLine2;
-    }
 
-    public String getDescriptionLine3() {
-        return descriptionLine3;
-    }
-
-    public void setDescriptionLine3(String descriptionLine3) {
-        this.descriptionLine3 = descriptionLine3;
-    }
-
-    public String getDescriptionLine4() {
-        return descriptionLine4;
-    }
-
-    public void setDescriptionLine4(String descriptionLine4) {
-        this.descriptionLine4 = descriptionLine4;
-    }
 
     public Integer getId() {
         return id;
@@ -84,6 +51,11 @@ public class HomePageCard {
     }
 
 
+    public String getDescription() {
+        return description;
+    }
 
-
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,13 +6,13 @@ values(10002,'Ravi', 'A1234568');
 
 
 insert into home_page_card
-values(1,'€99','test1','test2','test3','test4','1 week');
+values(1,'€30','It''s easier than you think , Just Book a table for 1 Week and We will help you sell it.','Become A Seller');
 
 insert into home_page_card
-values(2,'€55','test1','test2','test3','test4','2 weeks');
+values(2,'€55','Be it anything furniture , clothes or utensils .You can also book a table for 2 Weeks.','Everything Sells!');
 
 insert into home_page_card
-values(3,'€100','test1','test2','test3','test4','4 weeks');
+values(3,'€100','If you have a lot of good stuff to give away cause of renovation. Book a table for 1 month and save more.','Are You Renovating?');
 
 
 insert into role
