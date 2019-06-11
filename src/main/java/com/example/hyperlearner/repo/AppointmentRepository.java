8fef5164d9f44f796bf67c107abedac707a30931
Saurabh <tawaresaurabh@gmail.com>
2019-06-11 16:54:32 +0300

    Missed repo annotations

--- src/main/java/com/example/hyperlearner/repo/AppointmentRepository.java
+++ src/main/java/com/example/hyperlearner/repo/AppointmentRepository.java
@@ -2,6 +2,8 @@ package com.example.hyperlearner.repo;
 
 import com.example.hyperlearner.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("appointmentRepository")
 public interface AppointmentRepository extends JpaRepository<Appointment, String> {
 }
--- src/main/java/com/example/hyperlearner/repo/HomePageCardRepository.java
+++ src/main/java/com/example/hyperlearner/repo/HomePageCardRepository.java
@@ -2,7 +2,9 @@ package com.example.hyperlearner.repo;
 
 import com.example.hyperlearner.model.HomePageCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("homePageCardRepository")
 public interface HomePageCardRepository extends JpaRepository<HomePageCard, Integer> {
     //HomePageCard findById(Integer cardNumber);
 }
