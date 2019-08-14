90a54da0ae512573677b6d45d57c5f18b9597f28
Saurabh <tawaresaurabh@gmail.com>
2019-08-14 22:51:33 +0300

    Fixing build issue

--- src/main/java/com/example/hyperlearner/configuration/WebMvcConfig.java
+++ src/main/java/com/example/hyperlearner/configuration/WebMvcConfig.java
@@ -3,22 +3,20 @@ package com.example.hyperlearner.configuration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
+import java.util.Properties;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer  {
 
 
     @Bean(name = "localeResolver")
@@ -36,6 +34,25 @@ public class WebMvcConfig implements WebMvcConfigurer {
         messageResource.setCacheSeconds(1);
         messageResource.setUseCodeAsDefaultMessage(true);
         return messageResource;
+    }
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("tawaresaurabhopenload@gmail.com");
+        mailSender.setPassword("Zodiac@009");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 
     @Bean(name="localeInterceptor")
