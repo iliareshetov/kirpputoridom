97a630fa41073cf686676f1b52cccaeb19d1454f
Saurabh <tawaresaurabh@gmail.com>
2019-11-02 01:55:06 +0200

    added appointment delete and edit functionalities

--- src/main/java/com/example/hyperlearner/util/AppConstants.java
+++ src/main/java/com/example/hyperlearner/util/AppConstants.java
@@ -17,12 +17,15 @@ public class AppConstants {
     public static final String TH_CREATE_BOOKING_CARD_LINK = "/create_booking?week=";
     public static final String TH_VIEW_BOOKING_DETAIL_LINK = "/booking_detail?id=";
     public static final String TH_VIEW_BOOKING_CONFIRM_LINK = "/confirmBooking?id=";
+    public static final String TH_VIEW_BOOKING_DELETE_LINK = "/deleteBooking?id=";
+    public static final String TH_VIEW_BOOKING_EDIT_LINK = "/editBooking?id=";
 
 
 
     public static final String APPOINTMENT_REF_PREFIX = "APP";
     public static final String HOME_PAGE_TITLE = "Home";
     public static final String BOOKING_FORM_TITLE = "Booking form";
+    public static final String EDIT_BOOKING_FORM_TITLE = "Edit Booking";
     public static final String ADMIN_PANEL_TITLE = "Admin Panel";
 
 }
